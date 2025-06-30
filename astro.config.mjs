bea4d4b4132a276faf8dc9ff990c4cbbeddcb0c4
dario24git <89135258+dario24git@users.noreply.github.com>
2025-06-30 02:52:20 +0200

    Updated package.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import netlify from '@astrojs/netlify';
+import node from '@astrojs/node';
 
 export default defineConfig({
   integrations: [tailwind()],
   output: 'server',
-  adapter: netlify(),
+  adapter: node({
+    mode: 'standalone'
+  }),
   vite: {
     server: {
       hmr: {
