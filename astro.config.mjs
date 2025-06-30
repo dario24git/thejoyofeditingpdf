caffe75c133ef56a75e295412523284f6d241650
dario24git <89135258+dario24git@users.noreply.github.com>
2025-06-30 02:59:12 +0200

    Updated astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import cloudflare from '@astrojs/cloudflare';
+import netlify from '@astrojs/netlify';
 
 export default defineConfig({
   integrations: [tailwind()],
   output: 'server',
-  adapter: cloudflare({
-    platformProxy: {
-      enabled: true
-    }
-  }),
+  adapter: netlify(),
   vite: {
     server: {
       hmr: {
