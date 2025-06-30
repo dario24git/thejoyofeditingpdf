aa4d6b501f899e48bd36fb69cbbfa06d721dc2ee
dario24git <89135258+dario24git@users.noreply.github.com>
2025-06-30 02:56:45 +0200

    Updated package-lock.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import node from '@astrojs/node';
+import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   integrations: [tailwind()],
   output: 'server',
-  adapter: node({
-    mode: 'standalone'
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true
+    }
   }),
   vite: {
     server: {
